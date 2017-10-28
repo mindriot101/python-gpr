25cfa758c9453b07f52d5a855df5e1e7e4b9ecfd
Simon Walker <s.r.walker101@googlemail.com>
2017-10-28 22:53:33 +0100

    Add pybind11 to install requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,27 @@ from setuptools.command.build_ext import build_ext
 import sys
 import setuptools
 
+class GetPybindInclude(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
 ext_modules = [
     Extension(
         'gpr',
         ['src/gpr.cpp', 'vendor/gpr/src/parser.cpp', 'vendor/gpr/src/gcode_program.cpp'],
         include_dirs=[
-            '/usr/local/include',
+            GetPybindInclude(),
+            GetPybindInclude(user=True),
             'vendor/gpr/src',
         ],
         language='c++'
@@ -32,5 +47,6 @@ setup(
     name='gpr',
     ext_modules=ext_modules,
     zip_safe=False,
+    install_requires=['pybind11', ],
     cmdclass={'build_ext': BuildExt},
 )
