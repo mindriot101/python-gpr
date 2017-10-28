6831c14b78c4d6d202feab7cbfe854d05aba4dad
Simon Walker <s.r.walker101@googlemail.com>
2017-10-28 23:07:39 +0100

    Correct compilation

--- setup.py
+++ setup.py
@@ -20,7 +20,9 @@ class GetPybindInclude(object):
 ext_modules = [
     Extension(
         'gpr',
-        ['src/gpr.cpp', 'vendor/gpr/src/parser.cpp', 'vendor/gpr/src/gcode_program.cpp'],
+        ['src/gpr.cpp',
+         'vendor/gpr/src/parser.cpp',
+         'vendor/gpr/src/gcode_program.cpp'],
         include_dirs=[
             GetPybindInclude(),
             GetPybindInclude(user=True),
@@ -30,17 +32,57 @@ ext_modules = [
     ),
 ]
 
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.cpp') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14] compiler flag.
+    The c++14 is prefered over c++11 (when it is available).
+    """
+    if has_flag(compiler, '-std=c++14'):
+        return '-std=c++14'
+    elif has_flag(compiler, '-std=c++11'):
+        return '-std=c++11'
+    else:
+        raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                           'is needed!')
+
+
 class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+    }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
-        opts = []
-        opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-        opts.append('-std=c++11')
-
+        opts = self.c_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+        elif ct == 'msvc':
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
         for ext in self.extensions:
             ext.extra_compile_args = opts
-
         build_ext.build_extensions(self)
 
 setup(
